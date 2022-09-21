fb1c425753682e2ac09d16b09b06384740f9a40e
ashwabu <abdulazizredi90@gmail.com>
2022-09-21 11:31:01 +0300

    file add

---
+++ app/Http/Livewire/CartCountComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CartCountComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.cart-count-component');
+    }
+}
